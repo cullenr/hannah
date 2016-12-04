d400c1c938261deb6f432335055d9a7ec330ad8c
Rory Cullen <rory.cullen@imagination.com>
2016-12-04 15:05:03 +0000

    implemented new scrolling

--- main.lua
+++ main.lua
@@ -1,6 +1,4 @@
 local log = require "lib.log"
-log.level = "warn"
-
 local map = require "map-parser"
 local anim8 = require "lib.anim8"
 local time
@@ -9,7 +7,7 @@ local run
 
 local map_data
 local display = {
-    scale = 1,
+    scale = 2,
     size_pw = love.graphics.getWidth(),
     size_ph = love.graphics.getHeight() 
 }
@@ -17,15 +15,17 @@ local display = {
 love.graphics.setBackgroundColor(60, 90, 230)
 
 function love.load(args)
-	log.info("args", args)
     
     -- parse command line	
     for _, v in pairs(args) do
         if(v == "--verbose") then log.level = "trace" end
     end
+	
+    log.info("args", args, display)
 
     -- here we have some dummy data for the display
-    map_data = map:load("assets/maps/test.lua", display)
+    -- map_data = map:load("assets/maps/test.lua", display)
+    map_data = map:load("assets/maps/w1l1.lua", display)
 
     love.graphics.setDefaultFilter("nearest")
 	myShader = love.graphics.newShader[[
@@ -52,8 +52,26 @@ function love.load(args)
 	time = 0;
 end
 
+local view_px = 0
+local view_py = 0
+
 function love.update(dt)
-	run:update(dt)
+    if love.keyboard.isDown("escape") then love.event.quit() end
+    
+    if love.keyboard.isDown("a", "left") then
+        view_px = view_px - 10
+    elseif love.keyboard.isDown("d", "right") then
+        view_px = view_px + 10
+    end
+
+    if love.keyboard.isDown("w", "up") then
+        view_py = view_py - 10
+    elseif love.keyboard.isDown("s", "down") then
+        view_py = view_py + 10
+    end
+
+
+    run:update(dt)
 	time = time + dt;
 	local factor = math.abs(math.cos(time)); --so it keeps going/repeating
 	myShader:send("factor", factor)
@@ -61,11 +79,12 @@ end
 
 function love.draw()
 	love.graphics.setShader(myShader)
-	run:draw(hannah, 250, 100, 0, 8)
-	--love.graphics.draw(hannah, 250, 100, 0, 8, 8)
-	love.graphics.setShader()
-    
-    map:draw(map_data, 0, 0, display)
 
---love.event.quit(dd)
+    run:draw(hannah, 250, 64, 0, display.scale)
+    map:draw(map_data, view_px, view_py, display)
+	
+    love.graphics.setShader()
+    
+
+--    love.event.quit()
 end
